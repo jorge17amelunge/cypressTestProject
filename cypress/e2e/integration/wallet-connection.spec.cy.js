9df8bcdf95f02b21d283a37be9145d8e6962be0b
Jorge Amelunge <jamelunge@globalnexo.com>
2025-09-04 17:18:07 -0400

    Save changes to load test.

--- cypress/e2e/integration/wallet-connection.spec.cy.js
+++ cypress/e2e/integration/wallet-connection.spec.cy.js
@@ -11,23 +11,8 @@ describe('Test metamask connection', () => {
 		}
 		})
 
-		cy.get('[testdata-id="connect-button"]').should('be.visible').click()
+		cy.get('[testdata-id="connect-button"]').first().should('be.visible').click()
 
-		//cy.get('[data-testid="rk-wallet-option-io.metamask"]').click();
-		cy.connectToDapp();
-		//cy.wait(60000);
-		//cy.connectToDapp(['Account 1']);
-		//cy.get('.popover-content').within(() => {
-		//// Interact with the popup UI elements
-		//cy.get('[data-testid="your-button-selector"]').click();
-		//});
-		//cy.visit('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html');
-		//cy.window().then(win => {
-		//win.chrome.tabs.create({ url: `chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html#unlock` });
-		//	cy.get('[data-testid="unlock-password"]').type(password)
-		//});
-
-		//testdata-id="swap-from-input"
-		//testdata-id="swap-to-input"
+		cy.get('[data-testid="rk-wallet-option-io.metamask"]').click();
 	})
 })
